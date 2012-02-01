8091dcbef27b25d6153940e9612de952fa3a2d62
Edgar Solomonik <esolomon@hopper03.(none)>
2012-01-31 17:38:13 -0800

    New 1024 node script.

--- apsp/script4096_notau.sh
+++ apsp/script4096_notau.sh
@@ -2,11 +2,9 @@
 #PBS -q regular
 #PBS -V
 #PBS -l mppwidth=24576
-#PBS -l mppnppn=4
-#PBS -l mppdepth=6
 #PBS -j eo
-#PBS -o dcapsp.hopper.1024nodes.f.out
-#PBS -e dcapsp.hopper.1024nodes.f.out
+#PBS -o dcapsp.hopper.1024nodes.g.out
+#PBS -e dcapsp.hopper.1024nodes.g.out
 #PBS -l walltime=00:20:00
 #PBS -A mp156
 
@@ -25,7 +23,10 @@ aprun -n 4096 -N 4 -d 6 -ss ./test_dcapsp -pdim 32 -n 32768 -b1 16 -b2 32 -test 
 aprun -n 4096 -N 4 -d 6 -ss ./test_dcapsp -pdim 16 -n 4096 -b1 8 -b2 16 -test 0 -crep 16
 aprun -n 4096 -N 4 -d 6 -ss ./test_dcapsp -pdim 16 -n 8192 -b1 16 -b2 16 -test 0 -crep 16
 aprun -n 4096 -N 4 -d 6 -ss ./test_dcapsp -pdim 16 -n 16384 -b1 16 -b2 32 -test 0 -crep 16
+aprun -n 4096 -N 4 -d 6 -ss ./test_dcapsp -pdim 16 -n 32768 -b1 32 -b2 32 -test 0 -crep 16
 
 
 aprun -n 4096 -N 4 -d 6 -ss ./test_dcapsp -pdim 64 -n 65536 -b1 32 -b2 32 -test 0 -crep 1
+aprun -n 4096 -N 4 -d 6 -ss ./test_dcapsp -pdim 32 -n 65536 -b1 32 -b2 32 -test 0 -crep 4
+aprun -n 4096 -N 4 -d 6 -ss ./test_dcapsp -pdim 64 -n 131072 -b1 64 -b2 64 -test 0 -crep 1
 
